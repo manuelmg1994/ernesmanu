24afa973c805ebc262970a637cb0a860a230e9ab
Ernesto <Ernesto@ernexto_ab>
2016-03-10 13:06:41 +0100

    Modificacion de Ernesto

--- githubmanu/src/githubmanu/Githubmanu.java
+++ githubmanu/src/githubmanu/Githubmanu.java
@@ -16,6 +16,7 @@ public class Githubmanu {
      */
     public static void main(String[] args) {
         System.out.println("Modificacion de Ernesto");
+        System.out.println("Hola");
     }
     
 }
